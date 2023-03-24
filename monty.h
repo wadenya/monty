c2d7729e4fecb0d56bf88be116b7a7afdca06305
Maspecks <mwaniki_g@yahoo.com>
2023-03-24 01:09:52 +0000

    Function fies

--- monty.h
+++ monty.h
@@ -1,16 +1,13 @@
-#ifndef MONTY_H
-#define MONTY_H
+#ifndef __ MONTY_H__
+#define __MONTY_H__
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unlstd.h>
+#include <unistd.h>
 
 #define STACK 0
 #define QUEUE 1
-#define DELMIS " \n\t\a\b"
-
-/* GLOBAL OPCODE TOKENS */
-extern char **op_toks;
+#define DELIMS "\n\t\a\b"
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -23,9 +20,9 @@ extern char **op_toks;
  */
 typedef struct stack_s
 {
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
+        int n;
+        struct stack_s *prev;
+        struct stack_s *next;
 } stack_t;
 
 /**
@@ -38,19 +35,20 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
+        char *opcode;
+        void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
+/* Primary interpreter functions */
 void free_stack(stack_t **stack);
 int init_stack(stack_t **stack);
-int check_mode(stack_t *stack);
+int check_mode(stack_t **stack);
 void free_tokens(void);
 unsigned int token_arr_len(void);
 int run_monty(FILE *script_fd);
 void set_op_tok_error(int error_code);
 
-/* OPCODE FUNCTIONS */
+/*OPCODE FUNCTIONS*/
 void monty_push(stack_t **stack, unsigned int line_number);
 void monty_pall(stack_t **stack, unsigned int line_number);
 void monty_pint(stack_t **stack, unsigned int line_number);
@@ -65,24 +63,22 @@ void monty_mod(stack_t **stack, unsigned int line_number);
 void monty_pchar(stack_t **stack, unsigned int line_number);
 void monty_pstr(stack_t **stack, unsigned int line_number);
 void monty_rotl(stack_t **stack, unsigned int line_number);
-void monty_rotr(stack_t **stack, unsigned int line_number);
 void monty_stack(stack_t **stack, unsigned int line_number);
 void monty_queue(stack_t **stack, unsigned int line_number);
 
-/* CUSTOM STANDARD LIBRARY FUNC */
-char **strow(char *str, char *delims);
+/*CUSTOM STD LIBRARY FUNCTIONS*/
+char **strtow(char *str, char *delims);
 char *get_int(int n);
 
-/* ERROR MESSAGES & ERROR CODES */
+/*Error messages & codes*/
 int usage_error(void);
 int malloc_error(void);
 int f_open_error(char *filename);
 int unknown_op_error(char *opcode, unsigned int line_number);
 int no_int_error(unsigned int line_number);
 int pop_error(unsigned int line_number);
-int pint_error(unsigned int line_number);
 int short_stack_error(unsigned int line_number, char *op);
 int div_error(unsigned int line_number);
 int pchar_error(unsigned int line_number, char *message);
 
-#endif
+#endif /*__MONTY_H__*/
